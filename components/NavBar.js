3b42f02f8030ec88bbade7057865d0ea22a881b9
minsoo <minsu2344@naver.com>
2022-10-11 21:11:16 +0900

    <style jsx> 독립적 스타일링

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from './NavBar.module.css';
 
 export default function NavBar() {
   const router = useRouter();
@@ -8,13 +7,28 @@ export default function NavBar() {
   return(
     <nav>
       <Link href='/'>
-        {/* 백틱을 이용한 복수 className */}
-        <a className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>Home</a>
+        <a className={router.pathname==='/' ? 'active' : ''}>Home</a>
       </Link>
       <Link href='/about'>
-        {/* 배열과 join()을 이용한 복수 className */}
-        <a className={[styles.link, router.pathname === '/about' ? styles.active : ''].join(' ')}>About</a>
+        <a className={router.pathname==='/about' ? 'active' : ''}>About</a>
       </Link>
+      {/* style 태그에 jsx를 추가 후 {``}내부에 css와 동일하게 작성 */}
+      {/* 아래 style은 현재 컴포넌트에만 적용 */}
+      {/* 다른 곳에서 바꾸려 해도 적용 안 됨 */}
+      {/* 다른 곳의 다른 태그에도 적용 안 됨 */}
+      <style jsx>
+        {`          
+          nav {
+            background-color: tomato;
+          }
+          a {
+            text-decoration: none;
+          }
+          .active {
+            color: yellow;
+          }
+        `}
+      </style>
     </nav>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,17 @@ import NavBar from "../components/NavBar";
 export default function Home() {
   return (
     <div>
-      {/* NavBar 컴포넌트 리액트와 같은 방법으로 추가 */}
       <NavBar />
-      <h1>Hello</h1>
+      {/* active를 줘도 여기선 적용 안 됨 */}
+      <h1 className="active">Hello</h1>
+      <style jsx>
+        {/* 여기서 a 태그 바꾸려해도 NavBar에 적용 안 됨 */}
+        {`
+          a {
+            color: white;
+          }
+        `}
+      </style>
     </div>
   )
 }
