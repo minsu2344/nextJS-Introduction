07213d70fbd2853abe1d1074b01bc0f7d2bcafaf
minsoo <minsu2344@naver.com>
2022-10-17 11:35:07 +0900

    server side rendering으로 변경

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
-// '_'로 시작하는 파일은 렌더링 되기 전에 가장 먼저 확인하고 원하는 페이지 렌더링 함 => custom app
-
 import Layout from "../components/Layout";
 import '../styles/globals.css'
 
 export default function App({Component, pageProps}) {
   return (
-    // NavBar을 지워주고 Layout으로 감싸줌
-    // Layout의 children은 <Component />
+    // Home의 pageProps인 results가 전달됨
     <Layout>
       <Component {...pageProps} />
     </Layout>
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
 import { useEffect, useState } from "react";
 import Seo from "./Seo"
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
+// server side에서 return된 results 가져오기
+export default function Home({results}) {
+  // getServerSideProps 함수로 옮겨서 필요 없어짐
+  // const [movies, setMovies] = useState([]);
+  
+  // useEffect(() => {
+  //   (async () => {
+  //     const {results} = await (await fetch('/api/movies')).json();
 
-  useEffect(() => {
-    (async () => {
-      // fetch를 /api/movies로 변경
-      const {results} = await (await fetch('/api/movies')).json();
-
-      setMovies(results);
-    })();
-  }, []);
+  //     setMovies(results);
+  //   })();
+  // }, []);
 
   return (
     <div className="container">
       <Seo title='Home' />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie => (
+      {/* movies를 results로 변경 & Loading 제거 */}
+      {results?.map(movie => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -47,4 +48,19 @@ export default function Home() {
       </style>
     </div>
   )
+}
+
+// 함수 이름 절대 바꾸면 안 됨!!!
+// 함수에 코드를 쓰면 server side에서 돌아감(중요한 정보 숨기기 가능)
+export async function getServerSideProps() {
+  // useEffect에 있던 results를 가져옴
+  // client에서만 fetch 주소가 통함. server에서는 전체 주소 필요
+  const {results} = await (await fetch('http://localhost:3000/api/movies')).json();
+  // return으로 object를 반환. object에는 props라는 key 존재
+  // return한 object는 props를 통해 page로 보낼 수 있음
+  return {
+    props: {
+      results,
+    }
+  }
 }
